66ade9e5c642a3ea9eb35baf53613626360fb712
최희주 <gmlwnchl@naver.com>
2023-09-07 15:29:40 +0900

    interpark plane 데이터 쪼개기 완성

--- waffle/pythonbackend/waffle/models.py
+++ waffle/pythonbackend/waffle/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class InterparkPlaneModel(models.Model):
+    plane_company = models.TextField()
+
+    def __str__(self):
+        return self.plane_company
--- waffle/pythonbackend/waffle/views.py
+++ waffle/pythonbackend/waffle/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
-
-# Create your views here.
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 import json
-
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 @csrf_exempt
 def convert_data(request):
@@ -12,9 +14,34 @@ def convert_data(request):
         # Spring Boot로부터 데이터 수신
         data = json.loads(request.body)
 
-        # Spring Boot로 응답
-        response_data = {
-            'result': 'accept success'
-        }
+        url = 'https://fly.interpark.com/booking/mainFlights.do?tripType=OW&sdate0=20240221&sdate1=&dep0=PUS&arr0=NRT&dep1=&arr1=&adt=1&chd=0&inf=0&val=&comp=Y&via=#list'
+        xpath = '//ul[@id="schedule0List"]'
 
-        return HttpResponse(json.dumps(response_data), content_type="application/json")
+        options = Options()
+        options.add_argument("--headless")  # 헤드리스 모드로 실행
+        options.add_argument("--disable-gpu")  # GPU 사용 안 함 (헤드리스 모드에서 필요)
+
+        # Chrome WebDriver 실행 파일의 경로를 지정
+        driver = webdriver.Chrome()
+        try:
+            driver.get(url)
+
+            elements = driver.find_elements(By.XPATH, '//ul[@id="schedule0List"]/li')
+            # data = [element.get_attribute('textContent') for element in elements]
+            data = []
+            for element in elements:
+                origin = element.get_attribute('textContent').split()
+                data.append(origin)
+
+            response_data = {
+                'result': 'accept success',
+                'data' : data
+            }
+
+            # Spring Boot로 응답
+            json_response = json.dumps(response_data, ensure_ascii=False).encode('utf-8')
+            return HttpResponse(json_response, content_type="application/json;charset=utf-8")
+
+        finally:
+            driver.quit()
+
