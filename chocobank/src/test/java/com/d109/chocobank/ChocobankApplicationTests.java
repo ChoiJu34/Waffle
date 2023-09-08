9869b7263d94760b3f78359f6439b336de10a7a6
최가연 <cn01803@naver.com>
2023-09-08 14:24:55 +0900

    📰 Init: init bank project

---
+++ chocobank/src/test/java/com/d109/chocobank/ChocobankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.d109.chocobank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChocobankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
