b5b0e36e32789a134849a99d941ef83f06b85d41
정준우 <woojunjung18@gmail.com>
2023-09-11 17:25:46 +0900

    Fix: Update

--- waffle/reactfrontend/src/components/MainPage/MainPage.jsx
+++ waffle/reactfrontend/src/components/MainPage/MainPage.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import styled from 'styled-components';
 
 const MainPage = () => {
   return (
-    <div>
-        fd
-    </div>
+    <></>
   )
 }
 
+const HeaderWrapper = styled.div`
+`
 export default MainPage
