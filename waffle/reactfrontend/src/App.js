fcc3fb52440fc529b73949602ac9742e8faa2388
정준우 <woojunjung18@gmail.com>
2023-09-11 15:53:04 +0900

    Feat: 헤더 구현

--- waffle/reactfrontend/src/App.js
+++ waffle/reactfrontend/src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import { useMediaQuery } from 'react-responsive'
 
-function App() {
+import Header from './components/Header/Header.jsx'
+
+const App = () => {
+  const isMobile = useMediaQuery({query: '(max-width:768px)'});
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
     </div>
   );
 }
