7cab3a25b30de608fc330844b9b4c40e6e18aac4
정준우 <woojunjung18@gmail.com>
2023-09-11 17:49:55 +0900

    Feat: 메인페이지 사진 추가

--- waffle/reactfrontend/src/components/MainPage/MainPage.jsx
+++ waffle/reactfrontend/src/components/MainPage/MainPage.jsx
@@ -1,12 +1,20 @@
 import React from 'react'
 import styled from 'styled-components';
+import MainImg from '../../assets/MainImg.png'
 
 const MainPage = () => {
   return (
-    <></>
+      <MainPageWrapper>
+        <img src={MainImg} className="main-img"/>
+      </MainPageWrapper>
   )
 }
 
-const HeaderWrapper = styled.div`
+const MainPageWrapper = styled.div`
+  margin-top: 10px;
+
+  .main-img {
+    width: 100%;
+  }
 `
 export default MainPage
