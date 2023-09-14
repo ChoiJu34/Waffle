baca5182abea40b239c11bf7c18824a3b4f24116
최희주 <gmlwnchl@naver.com>
2023-09-14 16:45:41 +0900

    update : interpark Plane python, java 멀티 스레드, 프로세스 적용 성공
    Former-commit-id: 75050d9457872809695d37f7513ad39129e3d645

--- waffle/javabackend/src/main/java/com/d109/waffle/api/trippackage/service/TripPackageService.java
+++ waffle/javabackend/src/main/java/com/d109/waffle/api/trippackage/service/TripPackageService.java
@@ -7,7 +7,10 @@ import java.util.Map;
 import java.util.PriorityQueue;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.AsyncResult;
 import org.springframework.stereotype.Service;
+import org.springframework.util.concurrent.ListenableFuture;
 import org.springframework.web.client.RestTemplate;
 
 import com.d109.waffle.api.trippackage.dto.RecommendDto;
@@ -52,15 +55,18 @@ public class TripPackageService {
 	}
 
 	public Map<String, Object> all(RecommendDto recommendDto) throws JsonProcessingException {
-		Map<String, Object> map1 = interparkPlane(recommendDto);
-		Map<String, Object> map2 = interparkHotel(recommendDto);
-		Map<String, Object> result = new HashMap<>();
-		result.put("plane", map1);
-		result.put("hotel", map2);
-		return result;
+		ListenableFuture<Map<String, Object>> map1 = interparkPlane(recommendDto);
+		System.out.println("111");
+		ListenableFuture<Map<String, Object>> map2 = interparkPlane2(recommendDto);
+		Map<String, Object> ans = new HashMap<>();
+		map1.addCallback(result -> ans.put("plane", result), error -> System.out.println(error.getMessage()));
+		map2.addCallback(result -> ans.put("hotel", result), error -> System.out.println(error.getMessage()));
+		return ans;
 	}
 
-	public Map<String, Object> interparkPlane(RecommendDto recommendDto) throws JsonProcessingException {
+	@Async("multiAsync")
+	public ListenableFuture<Map<String, Object>> interparkPlane(RecommendDto recommendDto) throws JsonProcessingException {
+		System.out.println("start");
 		String s = restTemplate.postForObject("http://127.0.0.1:8000/interparkPlane", recommendDto, String.class);
 		ObjectMapper objectMapper = new ObjectMapper();
 		Map<String, Object> dataMap = objectMapper.readValue(s, new TypeReference<Map<String, Object>>() {});
@@ -68,34 +74,46 @@ public class TripPackageService {
 		// data:[{
 		// 	plane:[[[],[],[],[]],[],[]]
 		// 	card:[[[],[],[],[],[]],[],[]]},{...}]
-		for(int i=0; i<dataMap.size(); i++){//비행기 탑승 횟수
-			PriorityQueue<cardpq> pq = new PriorityQueue<>((o1, o2) -> (o1.planePrice-o2.planePrice));
-			List<Map<String,List<List<List<String>>>>> planPlane = (List<Map<String, List<List<List<String>>>>>)dataMap.get("data");
-			for(int k=0; k<planPlane.get(i).get("plane").size(); k++){//비행기 출발 날짜 범위
-				List<List<String>> planeList = planPlane.get(i).get("plane").get(k);//k번째 날 비행기 목록
-				List<List<String>> cardList = planPlane.get(i).get("card").get(k);
-				for(int j=0; j<planeList.size(); j++){//0:비행기 회사, 1:출발 위치, 2:시간, 3:도착위치, 4:시간, 5:경유, 6: 걸리는 시간, 7:걸리는 분
-					for(int r=1; r<cardList.get(j).size()-1; r++) {
-						pq.add(new cardpq(cardList.get(j).get(r).split(" ")[0], Integer.parseInt(cardList.get(j).get(r).split(" ")[1]),
-							Integer.parseInt(cardList.get(j).get(cardList.get(j).size()-1)), j, k));
-					}
-				}
-			}
-			cardpq info = pq.peek();
-			planeInfo.put("startPlace", planPlane.get(i).get("plane").get(info.planeDateK).get(info.planeListJ).get(1));
-			planeInfo.put("startTime", planPlane.get(i).get("plane").get(info.planeDateK).get(info.planeListJ).get(2));
-			planeInfo.put("endPlace", planPlane.get(i).get("plane").get(info.planeDateK).get(info.planeListJ).get(3));
-			planeInfo.put("endTime", planPlane.get(i).get("plane").get(info.planeDateK).get(info.planeListJ).get(4));
-			planeInfo.put("long", planPlane.get(i).get("plane").get(info.planeDateK).get(info.planeListJ).get(6) + planPlane.get(i).get("plane").get(info.planeDateK).get(info.planeListJ).get(6));
-			planeInfo.put("endCompany", planPlane.get(i).get("plane").get(info.planeDateK).get(info.planeListJ).get(0));
-			Map<String, Object> card = new HashMap<>();
-			card.put("cardCompany", info.cardName);
-			planeInfo.put("card", card);
-			planeInfo.put("originPrice", info.originPirce);
-			planeInfo.put("discountPrice", info.planePrice);
-			planeInfo.put("discount", info.planePrice/info.originPirce*100);
-		}
-		return planeInfo;
+		// for(int i=0; i<dataMap.size(); i++){//비행기 탑승 횟수
+		// 	PriorityQueue<cardpq> pq = new PriorityQueue<>((o1, o2) -> (o1.planePrice-o2.planePrice));
+		// 	List<Map<String,List<List<List<String>>>>> planPlane = (List<Map<String, List<List<List<String>>>>>)dataMap.get("data");
+		// 	for(int k=0; k<planPlane.get(i).get("plane").size(); k++){//비행기 출발 날짜 범위
+		// 		List<List<String>> planeList = planPlane.get(i).get("plane").get(k);//k번째 날 비행기 목록
+		// 		List<List<String>> cardList = planPlane.get(i).get("card").get(k);
+		// 		for(int j=0; j<planeList.size(); j++){//0:비행기 회사, 1:출발 위치, 2:시간, 3:도착위치, 4:시간, 5:경유, 6: 걸리는 시간, 7:걸리는 분
+		// 			for(int r=1; r<cardList.get(j).size()-1; r++) {
+		// 				pq.add(new cardpq(cardList.get(j).get(r).split(" ")[0], Integer.parseInt(cardList.get(j).get(r).split(" ")[1]),
+		// 					Integer.parseInt(cardList.get(j).get(cardList.get(j).size()-1)), j, k));
+		// 			}
+		// 		}
+		// 	}
+		// 	cardpq info = pq.peek();
+		// 	planeInfo.put("startPlace", planPlane.get(i).get("plane").get(info.planeDateK).get(info.planeListJ).get(1));
+		// 	planeInfo.put("startTime", planPlane.get(i).get("plane").get(info.planeDateK).get(info.planeListJ).get(2));
+		// 	planeInfo.put("endPlace", planPlane.get(i).get("plane").get(info.planeDateK).get(info.planeListJ).get(3));
+		// 	planeInfo.put("endTime", planPlane.get(i).get("plane").get(info.planeDateK).get(info.planeListJ).get(4));
+		// 	planeInfo.put("long", planPlane.get(i).get("plane").get(info.planeDateK).get(info.planeListJ).get(6) + planPlane.get(i).get("plane").get(info.planeDateK).get(info.planeListJ).get(6));
+		// 	planeInfo.put("endCompany", planPlane.get(i).get("plane").get(info.planeDateK).get(info.planeListJ).get(0));
+		// 	Map<String, Object> card = new HashMap<>();
+		// 	card.put("cardCompany", info.cardName);
+		// 	planeInfo.put("card", card);
+		// 	planeInfo.put("originPrice", info.originPirce);
+		// 	planeInfo.put("discountPrice", info.planePrice);
+		// 	planeInfo.put("discount", info.planePrice/info.originPirce*100);
+		// }
+		System.out.println("end");
+		return new AsyncResult<>(dataMap);
+	}
+
+	@Async("multiAsync")
+	public ListenableFuture<Map<String, Object>> interparkPlane2(RecommendDto recommendDto) throws JsonProcessingException {
+		System.out.println("start2");
+		String s = restTemplate.postForObject("http://127.0.0.1:8000/interparkPlane", recommendDto, String.class);
+		ObjectMapper objectMapper = new ObjectMapper();
+		Map<String, Object> dataMap = objectMapper.readValue(s, new TypeReference<Map<String, Object>>() {});
+		Map<String, Object> planeInfo = new HashMap<>();
+		System.out.println("end2");
+		return new AsyncResult<>(dataMap);
 	}
 
 	public Map<String, Object> interparkHotel(RecommendDto recommendDto) throws JsonProcessingException {
