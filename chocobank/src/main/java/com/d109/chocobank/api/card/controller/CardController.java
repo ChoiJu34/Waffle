d56c0a36763086e4f83bf3203028028d09038db3
최가연 <cn01803@naver.com>
2023-09-25 10:49:30 +0900

    Fix: 은행 카드 정보 들고올 때 uuid 인증 삭제

--- chocobank/src/main/java/com/d109/chocobank/api/card/controller/CardController.java
+++ chocobank/src/main/java/com/d109/chocobank/api/card/controller/CardController.java
@@ -37,19 +37,33 @@ public class CardController {
 
     // 서비스에서 카드 번호로 카드 정보 조회
     @PostMapping("/service")
-    public ResponseEntity<?> getCard(@RequestHeader("Authorization-uuid") String uuid, @RequestBody Map<String, String> map) throws Exception {
-        log.info("chocobank getCard controller");
+    public ResponseEntity<?> getCard(@RequestBody Map<String, String> map) throws Exception {
         Map<String, Object> result = new HashMap<>();
         try {
-            result.put("result", cardService.getCard(uuid, map.get("cardNumber")).getName());
+            result.put("result", cardService.getCard(map.get("cardNumber")).getName());
             result.put("message", "SUCCESS");
             return new ResponseEntity<>(result, HttpStatus.OK);
         } catch (Exception e) {
             log.error(e.getMessage());
             result.put("message", "FAIL");
-            return new ResponseEntity<>(result, HttpStatus.INTERNAL_SERVER_ERROR);
+            return new ResponseEntity<>(result, HttpStatus.ACCEPTED);
         }
     }
+    // @PostMapping("/service")
+    // public ResponseEntity<?> getCard(@RequestHeader("Authorization-uuid") String uuid, @RequestBody Map<String, String> map) throws Exception {
+    //     log.info("chocobank getCard controller");
+    //     Map<String, Object> result = new HashMap<>();
+    //     try {
+    //         result.put("result", cardService.getCard(uuid, map.get("cardNumber")).getName());
+    //         result.put("message", "SUCCESS");
+    //         return new ResponseEntity<>(result, HttpStatus.OK);
+    //     } catch (Exception e) {
+    //         log.error(e.getMessage());
+    //         result.put("message", "FAIL");
+    //         return new ResponseEntity<>(result, HttpStatus.INTERNAL_SERVER_ERROR);
+    //     }
+    // }
+
 
     // 은행에서 사용자 카드 리스트 조회
     @GetMapping("/list/user")
