a4a4ba79b32d59eb4437ce3e78b15fe25027383b
김근우 <twnkjsfagnji@gmail.com>
2023-09-26 17:13:42 +0900

    Update waffle/pythonbackend/waffle/hotel.py, waffle/pythonbackend/waffle/plane.py

--- waffle/pythonbackend/waffle/plane.py
+++ waffle/pythonbackend/waffle/plane.py
@@ -93,6 +93,7 @@ def crawling_multi_thread(info):
     chrome_options = webdriver.ChromeOptions()
     chrome_options.add_argument('headless')
     chrome_options.add_argument('window-size=1920x1080')
+    chrome_options.add_argument("disable-gpu")
     chrome_options.add_argument('--no-sandbox')
     chrome_options.add_argument('--disable-dev-shm-usage')
 
